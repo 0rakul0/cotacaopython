4126c824f9ebdcf0e0f1f70dbe9f2c206f2df889
ANJOS, J. S <0rakul0render@gmail.com>
2022-08-13 23:36:19 -0300

    att

--- datamine.py
+++ datamine.py
@@ -9,7 +9,7 @@ class datamine():
         self.valor_rendimento = None
         self.historico = []
 
-    def limpeza(self, texto):
+    def limpeza_real(self, texto):
         texto = texto.replace('R$', '')
         texto = texto.replace('.', '')
         texto = texto.replace(',', '.')
@@ -27,14 +27,31 @@ class datamine():
         return result
 
     def run(self, soup):
+        # valor da cota
+        valor_cota = soup.find('div', {'id':'stock-price'})
+        valor_cota = valor_cota.find('span', {'class':'price'}).text
+        valor_cota = self.limpeza_real(valor_cota)
+
+        #porcentagem de dividendos
+        porcentagem = soup.find('span', text=re.compile('Dividend Yield'))
+        valor_porcentagem = porcentagem.find_next_sibling('span', {'class':'indicator-value'}).text
+        valor_porcentagem = valor_porcentagem.replace(',', '.').replace('%','')
+        valor_porcentagem = valor_porcentagem.strip()
+        valor_porcentagem = float(valor_porcentagem)
+        situacao = 1-valor_porcentagem
+        if situacao > 0:
+            situacao_porcentagem = f'acima de 1% - vale investir - {valor_porcentagem}%'
+        else:
+            situacao_porcentagem = f'abaixo de 1% - segurar ou vender - {valor_porcentagem}%'
+
         descricao_rentabilidade = soup.find('span', text=re.compile('Valor Patrimonial'))
-        valor_cota = descricao_rentabilidade.find_next_sibling('span', {'class':'indicator-value'}).text
-        valor_cota = self.limpeza(valor_cota)
-        self.valor_cota = float(valor_cota)
+        valor_patrimonio = descricao_rentabilidade.find_next_sibling('span', {'class':'indicator-value'}).text
+        valor_patrimonio = self.limpeza_real(valor_patrimonio)
+        self.valor_cota = float(valor_patrimonio)
 
         descricao_rendimento = soup.find('span', text=re.compile('Último Rendimento'))
         rendimento = descricao_rendimento.find_next_sibling('span', {'class': 'indicator-value'}).text
-        rendimento = self.limpeza(rendimento)
+        rendimento = self.limpeza_real(rendimento)
         self.valor_rendimento = float(rendimento)
 
         #grafico das cotações
@@ -43,12 +60,12 @@ class datamine():
         historico = historico.replace("\\",'')
         historico = eval(historico)
         self.historico = historico
-        historico = historico[-10:]
+        historico = historico[-15:]
 
-        dict_recurso = {'VALOR_COTA':valor_cota, 'RENDIMENTO':rendimento, 'HISTORICO':historico}
+        dict_recurso = {'VALOR_COTA':valor_cota, 'VALOR_PATRIMONIO':valor_patrimonio,  'PORCENTAGEM_DIVIDENDOS':valor_porcentagem, 'PORCETAGEM_RENDIMENTO':situacao_porcentagem, 'RENDIMENTO':rendimento, 'HISTORICO':historico}
 
         return dict_recurso
 
 if __name__ == "__main__":
     dt = datamine()
-    dt.inicio("visc11")
+    dt.inicio("mxrf11")
