5aedfd790aefeead85710acc61aee1db6823caa6
ANJOS, J. S <0rakul0render@gmail.com>
2022-08-27 10:49:28 -0300

    tentativa de por a calculadora

--- datamine.py
+++ datamine.py
@@ -147,11 +147,17 @@ class datamine():
         """
         try:
             segmento = soup.find('span', text=re.compile('Segmento'))
-            segmento = segmento.find_next_sibling('span', {'class': 'description'}).text
-            segmento = segmento.strip()
+            if segmento:
+                segmento = segmento.find_next_sibling('span', {'class': 'description'}).text
+                segmento = segmento.strip()
+            else:
+                segmento = 'N/A'
         except:
             segmento = soup_2.find('h3', text=re.compile('Segmento'))
-            segmento = segmento.find_next_sibling('strong', {'class': 'value'}).text
+            if segmento:
+                segmento = segmento.find_next_sibling('strong', {'class': 'value'}).text
+            else:
+                segmento = 'N/A'
 
         # vinculos
         try:
@@ -210,118 +216,66 @@ class datamine():
 
     def so_site2(self, soup_2):
         # Nome da cota
-        nome_cota = soup_2.find('h2', {'class':'card-title'}).text
-        nome_cota = nome_cota.split(' ')
-        nome_cota = nome_cota[-1]
+        try:
+            nome_cota = soup_2.find('h2', {'class':'card-title'}).text
+            nome_cota = nome_cota.split(' ')
+            nome_cota = nome_cota[-1]
+        except:
+            nome_cota = self.nome_cotacao
+        try:
+            # Liquidez média diária
+            liq_media_day = soup_2.find('span', text=re.compile('Liq\. méd\. diária'))
+            liq_media_day = liq_media_day.find_parent('div').find('strong', {'class':'value'}).text
+            liq_media_day = self.limpeza_real(liq_media_day)
 
-        # Liquidez média diária
-        liq_media_day = soup_2.find('span', text=re.compile('Liq\. méd\. diária'))
-        liq_media_day = liq_media_day.find_parent('div').find('strong', {'class':'value'}).text
-        liq_media_day = self.limpeza_real(liq_media_day)
+            # valor da cota
+            valor_cota = soup_2.find('h3', text=re.compile('Valor atual'))
+            valor_cota = valor_cota.find_next_sibling('strong',{'class':'value'}).text
+            valor_cota = self.limpeza_real(valor_cota)
 
-        # valor da cota
-        valor_cota = soup_2.find('h3', text=re.compile('Valor atual'))
-        valor_cota = valor_cota.find_next_sibling('strong',{'class':'value'}).text
-        valor_cota = self.limpeza_real(valor_cota)
+            # porcentagem de dividendos
+            porcentagem_divid = soup_2.find('small', text=re.compile('Rendimento'))
+            porcentagem_divid = porcentagem_divid.find_next('b').text
+            porcentagem_divid = self.limpeza_real(porcentagem_divid)
 
-        # porcentagem de dividendos
-        porcentagem_divid = soup_2.find('small', text=re.compile('Rendimento'))
-        porcentagem_divid = porcentagem_divid.find_next('b').text
-        porcentagem_divid = self.limpeza_real(porcentagem_divid)
+            # ultimo rendimento
+            ultimo_rendimento = soup_2.find('span', text=re.compile('Último rendimento'))
+            ultimo_rendimento = ultimo_rendimento.find_next('div', {'class':'info'}).find('strong', {'class':'value'}).text
+            ultimo_rendimento = self.limpeza_real(ultimo_rendimento)
 
-        # ultimo rendimento
-        ultimo_rendimento = soup_2.find('span', text=re.compile('Último rendimento'))
-        ultimo_rendimento = ultimo_rendimento.find_next('div', {'class':'info'}).find('strong', {'class':'value'}).text
-        ultimo_rendimento = self.limpeza_real(ultimo_rendimento)
+            # pvp
+            valor_mercado_pv = None
+            valor_patrimonio_pv = None
+            valor_mercado_pv = soup_2.find('span', text=re.compile('Valor de mercado'))
+            valor_mercado_pv = valor_mercado_pv.find_next_sibling('span', {'class': 'sub-value'}).text
+            valor_mercado_pv = self.limpeza_real(valor_mercado_pv)
+            valor_patrimonio_pv = soup_2.find('span', text=re.compile('Patrimônio'))
+            valor_patrimonio_pv = valor_patrimonio_pv.find_next_sibling('span', {'class': 'sub-value'}).text
+            valor_patrimonio_pv = self.limpeza_real(valor_patrimonio_pv)
+            preco_por_acao = (valor_mercado_pv / valor_patrimonio_pv).real
 
-        # pvp
-        valor_mercado_pv = None
-        valor_patrimonio_pv = None
-        valor_mercado_pv = soup_2.find('span', text=re.compile('Valor de mercado'))
-        valor_mercado_pv = valor_mercado_pv.find_next_sibling('span', {'class': 'sub-value'}).text
-        valor_mercado_pv = self.limpeza_real(valor_mercado_pv)
-        valor_patrimonio_pv = soup_2.find('span', text=re.compile('Patrimônio'))
-        valor_patrimonio_pv = valor_patrimonio_pv.find_next_sibling('span', {'class': 'sub-value'}).text
-        valor_patrimonio_pv = self.limpeza_real(valor_patrimonio_pv)
-        preco_por_acao = (valor_mercado_pv / valor_patrimonio_pv).real
+            # setor
+            segmento = soup_2.find('h3', text=re.compile('Segmento'))
+            segmento = segmento.find_next_sibling('strong', {'class':'value'}).text
 
-        # setor
-        segmento = soup_2.find('h3', text=re.compile('Segmento'))
-        segmento = segmento.find_next_sibling('strong', {'class':'value'}).text
+            # vinculos
+            vinculos_list = []
+            vinculos = soup_2.find('b', text=re.compile('FIIs relacionadas:'))
+            vinculos = vinculos.find_next_sibling('div')
+            vinculos = vinculos.find_all('a')
+            for linha in vinculos:
+                linha = linha.get('href')
+                linha = linha.split('/')
+                linha = linha[-1]
+                vinculos_list.append(linha)
 
-        # vinculos
-        vinculos_list = []
-        vinculos = soup_2.find('b', text=re.compile('FIIs relacionadas:'))
-        vinculos = vinculos.find_next_sibling('div')
-        vinculos = vinculos.find_all('a')
-        for linha in vinculos:
-            linha = linha.get('href')
-            linha = linha.split('/')
-            linha = linha[-1]
-            vinculos_list.append(linha)
+            ht = historico()
+            hist_list = ht.historico_inicio(nome=nome_cota)
 
-        ht = historico()
-        hist_list = ht.historico_inicio(nome=nome_cota)
-
-        dict_recurso = {'NOME_COTA':nome_cota,'SEGMENTO':segmento,'VINCULOS':vinculos_list,'LIQUIDEZ_MEDIA':liq_media_day,'VALOR_COTA':valor_cota,'PORCENMTAGEM_DIVIDENDOS':porcentagem_divid,'ULTIMO_RENDIMENTO':ultimo_rendimento,'P/VP': preco_por_acao, 'VALOR_MERCADO':valor_mercado_pv, 'VALOR_PATRIMONIO':valor_patrimonio_pv, 'HISTORICO':hist_list}
-        return dict_recurso
-
-    def abaixo_de(self, min=None, max=None, rendimento=None, limit_liquidez=None, mes=None):
-        url = "https://www.fundsexplorer.com.br/funds"
-        headers = {
-            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/ 97.0.4692.99 Safari/537.36"}
-        site1 = requests.get(url, headers=headers)
-        soup_fundo1 = bs(site1.content, 'html.parser')
-
-        resultado = []
-        result = self.fund_cotas(soup_fundo1, valor_min=min, valor_max=max, rendimento=rendimento, limit_liquidez=limit_liquidez, mes=mes)
-        for x in result:
-            if x != None:
-                resultado.append(x)
-        result = resultado
-        return result
-
-    def executor(self, acao, valor_min=None, valor_max=None, rendimento=None, limit_liquidez=None, mes=None):
-        result = self.inicio(acao)
-        if result != None:
-            valor_acao = result['VALOR_COTA']
-            if valor_acao != 'N/A':
-                if valor_acao > valor_min.real and valor_acao < valor_max.real:
-                    data = result['ULTIMO_PG']
-                    if data != None:
-                        mes_corte = data.split(' - ')
-                        mes_corte = mes_corte[-1]
-                        mes_corte = mes_corte.split('/')
-                        mes_corte = mes_corte[1]
-                        corte = result['RENDIMENTO']
-                        liquedez = result['LIQUIDEZ_DIARIA']
-                        if corte > rendimento and liquedez > limit_liquidez and mes_corte == mes:
-                            try:
-                                if result['P/PV'] >= 1:
-                                    pvp = 'caro'
-                                else:
-                                    pvp = 'com desconto'
-                            except:
-                                pvp = None
-                            cota = result['LIQUIDEZ_DIARIA'], acao, result['VALOR_COTA'], corte, result['SEGMENTO'], data, result['P/PV'], pvp
-                            print(f'fundos entre R${valor_min} e R${valor_max}, {cota}')
-                            return {cota}
-
-    def fund_cotas(self, soup, valor_min=None, valor_max=None, rendimento=None, limit_liquidez=None, mes=None):
-        # lista de cotações
-        lista_cotas = []
-        # nome da cota
-        fundos = soup.find('section', {'id': 'fiis-list'})
-        fundos = fundos.find('div', {'class': 'row'})
-        fundos = fundos.find_all('span', {'class', 'symbol'})
-        for nome_fundos in fundos:
-            nome_fundos = nome_fundos.text
-            lista_cotas.append(nome_fundos)
-
-        execucao = Parallel(n_jobs=-1)(
-            delayed(self.executor)(acao=acao, valor_min=valor_min, valor_max=valor_max, rendimento=rendimento, limit_liquidez=limit_liquidez, mes=mes) for acao
-            in lista_cotas)
-        return execucao
+            dict_recurso = {'NOME_COTA':nome_cota,'SEGMENTO':segmento,'VINCULOS':vinculos_list,'LIQUIDEZ_MEDIA':liq_media_day,'VALOR_COTA':valor_cota,'PORCENMTAGEM_DIVIDENDOS':porcentagem_divid,'ULTIMO_RENDIMENTO':ultimo_rendimento,'P/VP': preco_por_acao, 'VALOR_MERCADO':valor_mercado_pv, 'VALOR_PATRIMONIO':valor_patrimonio_pv, 'HISTORICO':hist_list}
+            return dict_recurso
+        except:
+            return None
 
     def carteira_publica(self):
         carteira_df = pd.read_excel('./bi/minha_carteira.xlsx', header=0, sheet_name='Planilha1')
@@ -343,7 +297,7 @@ class datamine():
         return carteira_dict
 if __name__ == "__main__":
     dt = datamine()
-    ht = historico()
-    dt.inicio('rzag11')
+    # ht = historico()
+    # dt.inicio('rzag11')
     # dt.carteira_publica()
-    # dt.abaixo_de(min=0, max=100, rendimento=0.9, limit_liquidez=30000, mes='08')
+
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
 from datamine import *
 from historico import *
+from calculadora_cotacao import *
 
 app = FastAPI()
 dt = datamine()
 ht = historico()
+cc = calculadora_cotacao()
 
 @app.get("/")
 async def root():
     return {
         "msg":["use o nome da /fundos11/nome_cotação",
-               "para ver o historico separadamente /hist/{name}"]
+               "para ver o historico separadamente /hist/{name}",
+               " a calculadora de acoes pode quebrar, pelo alto custo de processamento\n exemplo de uso \n",
+               "/calculadora_cotacao/min=0&max=100&rendimento=0.9&limit_liquidez=30000&mes='08'"
+               ]
             }
 
 @app.get("/fundos11/{name}")
@@ -24,3 +29,7 @@ async def historico(name: str):
 @app.get("/carteira")
 async def carteira():
     return dt.carteira_publica()
+
+@app.get("/calculadora_cotacao/min={minimo}&max={maximo}&rendimento={rendimento}&limit_liquidez={limit_liquidez}&mes={mes}")
+async def calculadora_cotacao(minimo: float, maximo: float, rendimento: float, limit_liquidez: int, mes: str):
+    return cc.abaixo_de(min=minimo, max=maximo, rendimento=rendimento, limit_liquidez=limit_liquidez, mes=mes)
