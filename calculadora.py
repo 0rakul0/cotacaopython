63d7d11705b48d7204ad1b408cec6556c0a0e18f
ANJOS, J. S <0rakul0render@gmail.com>
2022-09-04 20:27:51 -0300

    att calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,46 +1,83 @@
+from calculadora_cotacao import calculadora_cotacao
 from datamine import *
 import re
 import numpy as np
+import pandas as pd
 
 class calc():
-    def igualdade(self, acoes, valor_disponivel):
+
+    def run(self, valor_disponivel=None,rendimento_corte=None, divid_min=None, valor_min=None, valor_max=None):
+        dataframe = pd.read_csv("./bi/dataframe_cotacao.tsv", sep='\t')
+        new_df = dataframe.copy()
+        saida_json = self.igualdade(df_acoes=new_df, valor_disponivel=valor_disponivel, rendimento_corte=rendimento_corte,divid_min=divid_min, minimo=valor_min, maximo=valor_max)
+        return saida_json
+
+    def igualdade(self,df_acoes=None, valor_disponivel=None, rendimento_corte=None, divid_min=None,minimo=None, maximo=None):
         """
         valores das cotas <= ao valor para gastar no total
         numerode_cotas*dividendos
         para maximizar usaremos os valores das cotas como negativo
         """
-
         rendimento_mes = 0
-        nome_acoes = re.compile("(APTO11)|(MXRF11)|(GAME11)|(RZAG11)|(TORD11)|(VGHF11)")
-        # nome_acoes = re.compile("(BNFS11)|(FAED11)|(FOFT11)|(IRIM11)|(JPPA11)|(PRSV11)|(TGAR11)")
-        # obj = [9.25, 9.99, 9.7, 10.23]
-        # lado_rendimento =[0.12, 0.12, 0.13, 0.14]
+        nome_acoes = re.compile("(APTO11)|(MXRF11)|(GAME11)|(RZAG11)|(BIME11)")
+        rendimento_acao = rendimento_corte
         nome = []
         obj = []
+        yid = []
         lado_rendimento = []
-        for acao in acoes:
-            if nome_acoes.search(acao[0]):
-                nome.append(acao[0])
-                obj.append(acao[1])
-                lado_rendimento.append(acao[2])
+        tipo_negocio = []
+        if divid_min == None:
+            divid__min = 0
+        for id, acao in df_acoes.iterrows():
+            preco_divid = acao['Dividendo']
+            preco_divid = preco_divid.replace('.', '')
+            preco_divid = preco_divid.replace(',', '.').split()
+            preco_divid = float(preco_divid[-1])
+            divid_yid = acao['DividendYield']
+            preco_acao = acao['Preço Atual']
+            try:
+                divid_yid = divid_yid.replace(',', '.').replace('%','').split()
+                divid_yid = float(divid_yid[-1])
+                preco_acao = preco_acao.replace('.', '').replace(',', '.').split()
+                preco_acao = float(preco_acao[-1])
+                if (preco_divid >= rendimento_acao and divid_yid >= divid_min) and (preco_acao >= minimo and preco_acao <= maximo):
+                    try:
+                        nome_acao = acao['Códigodo fundo']
+                        preco_acao = acao['Preço Atual']
+                        nome_negocio = acao['Setor']
+                        preco_acao = preco_acao.replace('.','')
+                        preco_acao = preco_acao.replace(',','.').split()
+                        preco_acao = float(preco_acao[-1])
 
+                        nome.append(nome_acao)
+                        obj.append(preco_acao)
+                        lado_rendimento.append(preco_divid)
+                        yid.append(divid_yid)
+                        tipo_negocio.append(nome_negocio)
+                    except:
+                        pass
+            except:
+                pass
         qte_cotas = []
-
         quanto_eu_tenho = valor_disponivel
-
         print(f'valor para investir = {quanto_eu_tenho}')
         rendimento = quanto_eu_tenho/len(nome)
         print(f'valor distribuido = {rendimento}')
+        cotacoes_a_comprar = []
+        valor_recebido_list = []
         for id, item in enumerate(obj):
             quantidade_cotas = rendimento/item
             qte_cotas.append(round(quantidade_cotas))
-            print(f'cota: {nome[id]}, qte: {round(quantidade_cotas)}')
+            compras = f'cota: {nome[id]}, qte: {round(quantidade_cotas)}'
+            cotacoes_a_comprar.append(compras)
         for n, valor in enumerate(lado_rendimento):
             rendimento_acao = qte_cotas[n]*valor
             rendimento_mes += rendimento_acao
-            print(f'valor recebido por acao da {nome[n]}, será de: {rendimento_acao}')
-        print({rendimento_mes})
-        return rendimento_mes
+            valor_recebido = f'{nome[n]},o rendimento será de: {rendimento_acao}'
+            valor_recebido_list.append(valor_recebido)
+
+        saida_json = dict(zip(cotacoes_a_comprar,valor_recebido_list))
+        return saida_json
 
     def otimizacao(self, valor_para_gastar=None):
         """
@@ -67,8 +104,10 @@ class calc():
 
 if __name__ == "__main__":
     cl = calc()
-    dt = datamine()
-    acoes = dt.abaixo_de(min=0, max=20, rendimento=0.1)
-    valor_disponivel = 500
-    cl.igualdade(acoes, valor_disponivel)
+    valor_disponivel = 3000
+    rendimento_corte = 0.5
+    divid_min = 1
+    minimo = 0
+    maximo = 50
+    cl.run(valor_min=minimo, valor_max=maximo, rendimento_corte=rendimento_corte, valor_disponivel=valor_disponivel, divid_min=divid_min)
     # cl.otimizacao(1200)
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from datamine import *
 from historico import *
-from calculadora_cotacao import *
+from calculadora import *
 
 app = FastAPI()
 dt = datamine()
 ht = historico()
-cc = calculadora_cotacao()
+cc = calc()
 
 @app.get("/")
 async def root():
@@ -14,7 +14,13 @@ async def root():
         "msg":["use o nome da /fundos11/nome_cotação",
                "para ver o historico separadamente /hist/{name}",
                " a calculadora de acoes pode quebrar, pelo alto custo de processamento\n exemplo de uso \n",
-               "/calculadora_cotacao/min=0&max=100&rendimento=0.9&limit_liquidez=30000&mes='08'"
+               "para saber o calculo de qual cotação comprar com uma divisão necessaria\n",
+               "min = start ou valor da cotação maxima\n",
+               "max = close ou valor da cotação maxima\n",
+               "rendimento_corte = referece ao valor de rentabilidade da cota\n",
+               "valor_disponivel = referece ao valor total para comprar as acoes\n",
+               "divid_min = referece ao valor em porcentagem de rentabilidade ou seja, quanto maior que 1 melhor\n",
+               "/calculadora_cotacao/min=0&max=100&rendimento=0.9&rendimento_corte=rendimento_corte&valor_disponivel=valor_disponivel&divid_min=divid_min"
                ]
             }
 
@@ -30,6 +36,6 @@ async def historico(name: str):
 async def carteira():
     return dt.carteira_publica()
 
-@app.get("/calculadora_cotacao/min={minimo}&max={maximo}&rendimento={rendimento}&limit_liquidez={limit_liquidez}&mes={mes}")
-async def calculadora_cotacao(minimo: float, maximo: float, rendimento: float, limit_liquidez: int, mes: str):
-    return cc.abaixo_de(min=minimo, max=maximo, rendimento=rendimento, limit_liquidez=limit_liquidez, mes=mes)
+@app.get("/calculadora/min={minimo}&max={maximo}&rendimento={rendimento_corte}&valor_disponivel={valor_disponivel}&divid_min={divid_min}")
+async def calculadora(minimo: float, maximo: float, rendimento_corte: float, valor_disponivel: float, divid_min:float):
+    return cc.run(valor_min=minimo, valor_max=maximo, rendimento_corte=rendimento_corte, valor_disponivel=valor_disponivel, divid_min=divid_min)
